0911ffccb00f9145a0f6114131a687eecf9a8796
reihanif <reihanfaizalhanif@gmail.com>
2024-07-18 00:46:44 +0700

    install bootstrap icons

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './assets/main.css'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap'
 import './assets/main.scss'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
