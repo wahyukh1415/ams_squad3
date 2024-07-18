0485c3c7bee9fa262dbae6617747e35bbd7aa6e2
riyandrmd <ryanda.rama10@gmail.com>
2024-07-18 23:54:59 +0700

    change router path

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: '/dashboard',
+      name: 'dashboard',
       component: Dashboard
     }
   ]
