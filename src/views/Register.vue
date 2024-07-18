96ede4756161a8c7a355ac3d16b07b3ca6b0e201
Rohmatullah Sony Wijaya <rsonywijaya@gmail.com>
2024-07-18 10:39:10 +0700

    add view register

---
+++ src/views/Register.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="register">
+        <h1>This is a register page</h1>
+        <button class="btn btn-primary">Submit</button>
+    </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+    .home {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
