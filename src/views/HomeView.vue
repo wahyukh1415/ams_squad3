d975b210e00fe779a2981125ae114ca23095f563
Rohmatullah Sony Wijaya <rsonywijaya@gmail.com>
2024-07-19 22:47:52 +0700

    coba conflict & resolve

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,20 +2,22 @@
 import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
-const router = useRouter()
-const authUser = ref(JSON.parse(localStorage.getItem('auth-user')))
+const router = useRouter();
+const authUser = ref(JSON.parse(localStorage.getItem('auth-user')));
 
 onMounted(() => {
-    if(!localStorage.getItem('token') && !localStorage.getItem('auth-user')) {
-        router.push({name: 'login'})
+    if (!localStorage.getItem('token') && !localStorage.getItem('auth-user')) {
+        router.push({ name: 'login' });
     }
-})
+});
 
 function logout() {
-    localStorage.removeItem('token')
-    localStorage.removeItem('auth-user')
+    localStorage.removeItem('token');
+    localStorage.removeItem('auth-user');
 
-    router.push({name: 'login'}).then(() => { router.go(0) })
+    router.push({ name: 'login' }).then(() => {
+        router.go(0);
+    });
 }
 </script>
 
@@ -24,6 +26,7 @@ function logout() {
         <h1 v-if="authUser" class="w-100">Welcome {{ authUser.name }}, this is homepage</h1>
         <div>
             <button class="btn btn-primary" @click="logout">Logout</button>
+            <button class="btn btn-primary" @click="logout">conflict</button>
         </div>
         <div>
             <button class="btn btn-primary">Colek Sony</button>
