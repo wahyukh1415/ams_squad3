cc8c5fbd00a553c7b22c42cdfb15e3ee55bffff2
reihanif <161105975+reihanif@users.noreply.github.com>
2024-07-19 21:58:58 +0700

    feat/user-profile (#5)
    Co-authored-by: unknown <farhanbsyr@gmail.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import UserView from "../components/UserProfile.vue";
 import LoginView from '../views/LoginView.vue'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/user-profile",
+      name: "profile",
+      component: UserView,
+    },
       path: '/register',
       name: 'register',
       component: () => import('../views/Register.vue'),
@@ -14,11 +32,6 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
     },
     {
       path: '/register-seller',
@@ -38,4 +51,4 @@ const router = createRouter({
   ]
 })
 
-export default router
+export default router;
