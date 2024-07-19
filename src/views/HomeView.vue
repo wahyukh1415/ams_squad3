3da70ba88c0406cde4d27cbcf1985c9f8a67094b
Rohmatullah Sony Wijaya <rsonywijaya@gmail.com>
2024-07-19 23:02:53 +0700

    test resolve conflict

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -29,8 +29,8 @@ function logout() {
             <button class="btn btn-primary" @click="logout">conflict</button>
         </div>
         <div>
-            <button class="btn btn-primary">Colek Rehan</button>
-            <button class="btn btn-info">Colek Sony</button>
+            <button class="btn btn-danger">Colek Rehan</button>
+            <button class="btn btn-danger border border-1">Colek Sony</button>
         </div>
     </div>
 </template>
