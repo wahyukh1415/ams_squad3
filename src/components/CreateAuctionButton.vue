1e595cb9aec149414f14527760f19f8905ed225f
Rohmatullah Sony Wijaya <rsonywijaya@gmail.com>
2024-07-21 23:56:00 +0700

    move button create auction to component

---
+++ src/components/CreateAuctionButton.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { onMounted } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useAuthStore } from '@/stores/auth';
+import { RouterLink } from 'vue-router';
+
+const { authUser } = storeToRefs(useAuthStore());
+const { authCheck, logout } = useAuthStore();
+
+onMounted(() => {
+    authCheck();
+});
+</script>
+
+<template>
+    <div class="cta-wrapper">
+        <router-link to="/create-auction" class="btn btn-primary cta-seller">
+            <i class="bi bi-plus-circle-fill icon-auction"></i>
+            <span class="ms-2">Create New Auction</span>
+        </router-link>
+    </div>
+</template>
+
+<style scoped>
+.cta-seller {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    right: 0;
+    bottom: 0;
+    position: fixed;
+    margin: 30px;
+    cursor: pointer;
+    z-index: 999;
+    border: 1px solid #1c5fa8;
+}
+
+.icon-auction {
+    font-size: 30px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,7 @@ import HomeComingAuction from '@/components/HomeComingAuction.vue';
 import HomeEndedAuction from '@/components/HomeEndedAuction.vue';
 import Footer from '@/components/Footer.vue';
 import Navbar from '@/components/Navbar.vue';
+import CreateAuctionButton from '@/components/CreateAuctionButton.vue';
 
 import { RouterLink } from 'vue-router';
 
@@ -24,9 +25,8 @@ onMounted(() => {
     <HomeActiveAuction />
     <HomeComingAuction />
     <HomeEndedAuction />
-    <router-link to="/create-auction" class="btn btn-primary cta-seller d-flex flex-row align-items-center" style="right: 0; bottom: 0; position: fixed; margin: 30px; cursor: pointer; z-index: 999; border: 1px solid #1c5fa8">
-        <i class="bi bi-plus-circle-fill icon-auction" style="font-size: 30px"></i>
-        <span class="ms-2">Create New Auction</span>
-    </router-link>
+    <CreateAuctionButton />
     <Footer />
 </template>
+
+<style scoped></style>
