ae8bc650c3dca8475f5389a1ebffe4d63f4b8f36
riyandrmd <ryanda.rama10@gmail.com>
2024-07-23 04:34:43 +0700

    create carousel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,6 +10,7 @@ import Navbar from "@/components/Navbar.vue";
 import CreateAuctionButton from "@/components/CreateAuctionButton.vue";
 import FAQ from "@/components/FaqTemp.vue";
 import AuctionRules from "@/components/AuctionRules.vue";
+import Carousel from "@/components/Carousel.vue";
 
 import { RouterLink } from "vue-router";
 import AuctionRulesVue from "@/components/AuctionRules.vue";
@@ -18,48 +19,47 @@ const { authUser } = storeToRefs(useAuthStore());
 const { authCheck, logout } = useAuthStore();
 
 onMounted(() => {
-    authCheck();
+  authCheck();
 });
 </script>
 
 <template>
-    <Navbar />
-    <section id="hero" class="container">
-        <div class="row px-4 px-md-0">
-            <h1 class="heading col-12 col-md-6">Bids Beyond Boundaries</h1>
-            <div class="col-12 col-md-6 ps-md-5 d-flex align-items-center">
-                <div>
-                    <p class="paragraph text-secondary">
-                        Discover new auctions, place your bids, and uncover
-                        unique treasures. Dive in now to explore exciting
-                        opportunities and win big rewards!
-                    </p>
-                    <a class="btn btn-primary" href="#active-auction"
-                        >Explore now</a
-                    >
-                </div>
-            </div>
+  <Navbar />
+  <Carousel />
+  <section id="hero" class="container">
+    <div class="row px-4 px-md-0">
+      <h1 class="heading col-12 col-md-6">Bids Beyond Boundaries</h1>
+      <div class="col-12 col-md-6 ps-md-5 d-flex align-items-center">
+        <div>
+          <p class="paragraph text-secondary">
+            Discover new auctions, place your bids, and uncover unique
+            treasures. Dive in now to explore exciting opportunities and win big
+            rewards!
+          </p>
+          <a class="btn btn-primary" href="#active-auction">Explore now</a>
         </div>
-    </section>
-    <HomeActiveAuction />
-    <HomeComingAuction />
-    <AuctionRules />
-    <FAQ />
-    <CreateAuctionButton />
-    <Footer />
+      </div>
+    </div>
+  </section>
+  <HomeActiveAuction />
+  <HomeComingAuction />
+  <AuctionRules />
+  <FAQ />
+  <CreateAuctionButton />
+  <Footer />
 </template>
 
 <style scoped>
 #hero {
-    padding: 6rem 0;
+  padding: 6rem 0;
 }
 
 .heading {
-    font-weight: 900;
-    font-size: calc(2rem + 1.5vw);
+  font-weight: 900;
+  font-size: calc(2rem + 1.5vw);
 }
 
 .paragraph {
-    font-weight: 600;
+  font-weight: 600;
 }
 </style>
