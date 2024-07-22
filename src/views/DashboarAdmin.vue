c9dc8d72af1fa7d84957a20e3460a3277a1c7cd0
riyandrmd <ryanda.rama10@gmail.com>
2024-07-22 14:13:04 +0700

    minor update

--- src/views/DashboarAdmin.vue
+++ src/views/DashboarAdmin.vue
@@ -1,29 +1,31 @@
 <script setup>
-import User from '../components/tableUser.vue';
-import ListAuction from '../components/tableAuction.vue';
-import { useAuthStore } from '@/stores/auth';
-import { onMounted } from 'vue';
-import Footer from '@/components/Footer.vue';
-import Navbar from '@/components/Navbar.vue';
-import { storeToRefs } from 'pinia';
-import CreateAuctionButton from '@/components/CreateAuctionButton.vue';
+import User from "../components/tableUser.vue";
+import ListAuction from "../components/tableAuction.vue";
+import { useAuthStore } from "@/stores/auth";
+import { onMounted } from "vue";
+import Footer from "@/components/Footer.vue";
+import Navbar from "@/components/Navbar.vue";
+import { storeToRefs } from "pinia";
+import CreateAuctionButton from "@/components/CreateAuctionButton.vue";
 
 const { authCheck, logout } = useAuthStore();
 const { authUser } = storeToRefs(useAuthStore());
 
 onMounted(() => {
-    authCheck();
+  authCheck();
 });
 </script>
 
 <template>
-    <div class="container">
-        <h1>Dashboar Admin</h1>
-        <h4>List User</h4>
-        <User />
-        <br />
-        <h4>List Auction</h4>
-        <ListAuction />
-        <Footer />
-    </div>
+  <div>
+    <main class="container">
+      <h1>Dashboar Admin</h1>
+      <h4>List User</h4>
+      <User />
+      <br />
+      <h4>List Auction</h4>
+      <ListAuction />
+    </main>
+    <Footer />
+  </div>
 </template>
