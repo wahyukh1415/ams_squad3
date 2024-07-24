25ab6f500c293cc57a98f21f662fd6c7cae3e388
reihanif <reihanfaizalhanif@gmail.com>
2024-07-24 09:57:45 +0700

    update syntax

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -3,7 +3,7 @@ import faq from '@/assets/data/faq.json'
 import { reactive } from 'vue';
 import IllustrationFaq from './illustrations/IllustrationFaq.vue';
 
-const faqs = reactive(faq)
+const faqs = faq
 </script>
 <template>
     <section id="faq" class="py-5">
@@ -39,57 +39,6 @@ const faqs = reactive(faq)
             </div>
         </div>
     </section>
-
-
-
-    <!-- <div class="faq-container">
-        <div class="faq-title">
-            <h1>Frequently Asked Questions (FAQ)</h1>
-        </div>
-        <div class="faq-body">
-            <div class="faq-item">
-                <h5>1. What is BidBuddy?</h5>
-                <p>
-                    BidBuddy is a platform that allows users to bid on auction
-                    items. Or you can also sell items through the auction.
-                </p>
-            </div>
-            <div class="faq-item">
-                <h5>2. How to use BidBuddy?</h5>
-                <p>
-                    First, you need to create an account. Then, you can bid on
-                    auction items after you logged in to our website as a buyer.
-                    Or you can sell items through the auction after logged in to
-                    our website as a seller.
-                </p>
-            </div>
-            <div class="faq-item">
-                <h5>3. Is BidBuddy safe?</h5>
-                <p>
-                    BidBuddy is a safe auction platform that does not allow
-                    fraudulent activities. BidBuddy also provides a secure
-                    payment system so you don't have to worry about online
-                    scams.
-                </p>
-            </div>
-            <div class="faq-item">
-                <h5>4. Is there any cost to bid on items?</h5>
-                <p>
-                    In BidBuddy, there is no cost to bid on auction items. You
-                    just have to pay for the item that you want to bid on.
-                </p>
-            </div>
-            <div class="faq-item">
-                <h5>5. How do I sell items through the auction?</h5>
-                <p>
-                    After logging in as a seller, click the "Create New Auction"
-                    button, fill in the item details to be sold, including
-                    description, photos, starting price, and auction duration.
-                    Then, click "Submit" to start the auction.
-                </p>
-            </div>
-        </div>
-    </div> -->
 </template>
 
 <style scoped>
