b4cab70591e066b14ca307acf4133b161bad35c5
riyandrmd <ryanda.rama10@gmail.com>
2024-07-25 11:54:41 +0700

    minor update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,9 +7,10 @@ import Logo from "../components/logo/Logo.vue";
       <div class="row">
         <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
           <Logo class="w-50 text-primary-700" />
-          <p>
+          <p class="size">
             Jl. Medan Merdeka Barat No.6, RT.2/RW.3, Gambir, Kecamatan Gambir,
-            <br /> Kota Jakarta Pusat, Daerah Khusus Jakarta 10110.<br />
+            <br />
+            Kota Jakarta Pusat, Daerah Khusus Jakarta 10110.<br />
             <a href="mailto: emailfiktifgan@email.com"> bidbuddy@email.com </a>
           </p>
         </div>
@@ -17,18 +18,20 @@ import Logo from "../components/logo/Logo.vue";
           <h5 class="fw-bold text-primary-600">Our Social Media</h5>
           <ul class="list-unstyled mb-0 d-flex gap-4">
             <li>
-              <a href="#!" class="text-body"><i class="bi bi-instagram"></i></a>
+              <a href="#!" class="link">
+                <i class="bi bi-instagram"></i>
+              </a>
             </li>
             <li>
-              <a href="#!" class="text-body"><i class="bi bi-twitter-x"></i></a>
+              <a href="#!" class="link"><i class="bi bi-twitter-x"></i></a>
             </li>
             <li>
-              <a href="#!" class="text-body"><i class="bi bi-snapchat"></i></a>
+              <a href="#!" class="link"><i class="bi bi-snapchat"></i></a>
             </li>
           </ul>
         </div>
         <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-          <p class="mb-0">BidBuddy is protected and supervised by :</p>
+          <p class="mb-0 size">BidBuddy is protected and supervised by :</p>
           <img
             src="../assets/images/OJK_logo.png"
             alt="ojk logo.pbg"
@@ -44,7 +47,26 @@ import Logo from "../components/logo/Logo.vue";
 </template>
 
 <style scoped>
-.bi {
-  font-size: 32px;
+.link {
+  width: 40px;
+  height: 40px;
+  font-size: 18px;
+  color: #000;
+  border-radius: 50%;
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.15);
+  transition: box-shadow 0.3s ease;
+}
+
+.link:hover {
+  font-size: 20px;
+}
+
+.size {
+  font-size: 12px;
 }
 </style>
