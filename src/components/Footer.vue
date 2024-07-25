afd0bca38e1272e268d9672dcb2a565049fcdfd7
riyandrmd <ryanda.rama10@gmail.com>
2024-07-25 12:18:33 +0700

    update minor

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,8 +6,8 @@ import Logo from "../components/logo/Logo.vue";
     <div class="container p-4">
       <div class="row">
         <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-          <Logo class="w-50 text-primary-700" />
-          <p class="size">
+          <Logo class="width text-primary-700" />
+          <p class="size my-2">
             Jl. Medan Merdeka Barat No.6, RT.2/RW.3, Gambir, Kecamatan Gambir,
             <br />
             Kota Jakarta Pusat, Daerah Khusus Jakarta 10110.<br />
@@ -16,11 +16,9 @@ import Logo from "../components/logo/Logo.vue";
         </div>
         <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
           <h5 class="fw-bold text-primary-600">Our Social Media</h5>
-          <ul class="list-unstyled mb-0 d-flex gap-4">
+          <ul class="list-unstyled mb-0 d-flex justify-content-center justify-content-md-start gap-4">
             <li>
-              <a href="#!" class="link">
-                <i class="bi bi-instagram"></i>
-              </a>
+              <a href="#!" class="link"><i class="bi bi-instagram"></i></a>
             </li>
             <li>
               <a href="#!" class="link"><i class="bi bi-twitter-x"></i></a>
@@ -35,7 +33,7 @@ import Logo from "../components/logo/Logo.vue";
           <img
             src="../assets/images/OJK_logo.png"
             alt="ojk logo.pbg"
-            class="w-75"
+            class="width"
           />
         </div>
       </div>
@@ -68,5 +66,9 @@ import Logo from "../components/logo/Logo.vue";
 
 .size {
   font-size: 12px;
+}
+
+.width {
+  max-width: 75%;
 }
 </style>
