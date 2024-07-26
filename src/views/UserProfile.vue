0012fb9fc2f1f0e5dddd8f132f4bb6f7c6b7cc2a
unknown <farhanbsyr@gmail.com>
2024-07-26 15:04:19 +0700

    memperbaiki style di user profile

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -10,164 +10,159 @@ const user = JSON.parse(data);
 const avatarProfile = ref("");
 
 const generateAvatar = (name) => {
-    return `https://ui-avatars.com/api/?name=${encodeURIComponent(
-        name
-    )}&rounded=true&background=f5f7fa&color=5277a5`;
+  return `https://ui-avatars.com/api/?name=${encodeURIComponent(
+    name
+  )}&rounded=true&background=f5f7fa&color=5277a5`;
 };
 console.log(user.name);
 onMounted(() => {
-    avatarProfile.value = generateAvatar(user.name);
+  avatarProfile.value = generateAvatar(user.name);
 });
 </script>
 
 <template>
-    <Navbar />
-    <div class="wrapper-profile">
-        <div class="jumbroton d-flex flex-column align-items-center">
-            <div
-                class="image-profile h-100 d-flex gap-3 justify-content-center flex-column align-items-center"
-            >
-                <img
-                    :src="avatarProfile"
-                    width="100"
-                    height="100"
-                    alt="profile"
-                />
+  <Navbar />
+  <div class="wrapper-profile">
+    <div class="jumbroton d-flex flex-column align-items-center">
+      <div
+        class="image-profile h-100 d-flex gap-3 justify-content-center flex-column align-items-center"
+      >
+        <img :src="avatarProfile" width="100" height="100" alt="profile" />
 
-                <h2>{{ authUser.name }}</h2>
-            </div>
-            <ul class="list-profile d-flex gap-5">
-                <li v-if="authUser.role === 'SELLER'">Selling</li>
-                <li v-else-if="authUser.role === 'BUYER'">Bidding</li>
-                <li v-else>Dashboard</li>
-                <li>Interest</li>
-                <li class="active">Settings</li>
-            </ul>
-        </div>
-        <!-- settings section -->
-        <ProfileSettings />
-        <!-- akhir settings -->
+        <h2>{{ authUser.name }}</h2>
+      </div>
+      <ul class="list-profile d-flex gap-5">
+        <li v-if="authUser.role === 'SELLER'">Selling</li>
+        <li v-else-if="authUser.role === 'BUYER'">Bidding</li>
+        <li v-else>Dashboard</li>
+        <li>Interest</li>
+        <li class="active">Settings</li>
+      </ul>
     </div>
+    <!-- settings section -->
+    <ProfileSettings />
+    <!-- akhir settings -->
+  </div>
 </template>
 
 <style scoped>
 li {
-    list-style: none;
+  list-style: none;
 }
 ul {
-    padding: 0;
+  padding: 0;
 }
 .wrapper-profile {
-    font-family: "Poppins", sans-serif;
+  font-family: "Poppins", sans-serif;
 }
 
 .jumbroton {
-    background-image: url("../assets/images/monopoli-money.png");
-    color: white;
-    height: 300px;
-    padding-top: 63px;
+  background-image: url("../assets/images/monopoli-money.png");
+  color: white;
+  height: 300px;
+  padding-top: 63px;
 }
 .image-profile h2 {
-    font-size: 32px;
-    font-weight: 400;
+  font-size: 32px;
+  font-weight: 400;
 }
 .list-profile li {
-    font-size: 18px;
-    cursor: pointer;
-    font-weight: 600;
-    position: relative; /* Untuk mengatur posisi relatif untuk garis bawah */
-    transition: color 0.3s ease; /* Transisi untuk perubahan warna teks */
+  font-size: 18px;
+  cursor: pointer;
+  font-weight: 600;
+  position: relative; /* Untuk mengatur posisi relatif untuk garis bawah */
+  transition: color 0.3s ease; /* Transisi untuk perubahan warna teks */
 }
 
 .list-profile li::after {
-    content: "";
-    position: absolute;
-    left: 50%;
-    bottom: -1rem;
-    width: 120%;
-    height: 6px; /* Atur tinggi garis bawah */
-    background-color: transparent;
-    transition: background-color 0.3s ease; /* Transisi untuk perubahan warna garis bawah */
-    transform: translateX(-50%);
+  content: "";
+  position: absolute;
+  left: 50%;
+  bottom: -1rem;
+  width: 120%;
+  height: 6px; /* Atur tinggi garis bawah */
+  background-color: transparent;
+  transition: background-color 0.3s ease; /* Transisi untuk perubahan warna garis bawah */
+  transform: translateX(-50%);
 }
 
 .list-profile li:hover::after {
-    background-color: white; /* Warna garis bawah saat dihover */
+  background-color: white; /* Warna garis bawah saat dihover */
 }
 
 /* setting */
 .nav-settings {
-    margin-bottom: 48px;
+  margin-bottom: 48px;
 }
 .nav-settings h3 {
-    position: relative; /* Untuk mengatur posisi relatif untuk garis bawah */
-    transition: color 0.3s ease; /* Transisi untuk perubahan warna teks */
-    font-size: 18px;
-    display: inline;
+  position: relative; /* Untuk mengatur posisi relatif untuk garis bawah */
+  transition: color 0.3s ease; /* Transisi untuk perubahan warna teks */
+  font-size: 18px;
+  display: inline;
 }
 .nav-settings h3::after {
-    content: "";
-    position: absolute;
-    left: 0;
-    bottom: -0.5rem;
-    width: 110%;
-    height: 4px; /* Atur tinggi garis bawah */
-    background-color: black;
-    transition: background-color 0.3s ease; /* Transisi untuk perubahan warna garis bawah */
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: -0.5rem;
+  width: 110%;
+  height: 4px; /* Atur tinggi garis bawah */
+  background-color: black;
+  transition: background-color 0.3s ease; /* Transisi untuk perubahan warna garis bawah */
 }
 
 .detail-profile .profile-head h3 {
-    font-size: 14px;
+  font-size: 14px;
 }
 
 .profile-group {
-    font-size: 14px;
+  font-size: 14px;
 }
 .profile-group p {
-    margin: 0;
+  margin: 0;
 }
 .line-profile-group {
-    background-color: rgba(0, 0, 0, 0.1);
-    width: 100%;
-    height: 1px;
+  background-color: rgba(0, 0, 0, 0.1);
+  width: 100%;
+  height: 1px;
 }
 .btn-edit button {
-    border: none;
-    font-size: 14px;
-    background-color: transparent;
+  border: none;
+  font-size: 14px;
+  background-color: transparent;
 }
 .btn-edit button:hover {
-    border-bottom: 1px solid black;
+  border-bottom: 1px solid black;
 }
 .profile-head h3 {
-    margin: 0;
-    letter-spacing: 2px;
-    color: #666666;
-    font-weight: 300;
+  margin: 0;
+  letter-spacing: 2px;
+  color: #666666;
+  font-weight: 300;
 }
 
 /* modal */
 #form-modal {
-    display: flex;
-    flex-direction: column;
-    gap: 24px;
-    margin-bottom: 12px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  margin-bottom: 12px;
 }
 .form-group label {
-    position: absolute;
-    padding: 0 5px;
-    font-size: 12px;
-    top: -10px;
-    left: 10px;
-    background-color: white;
+  position: absolute;
+  padding: 0 5px;
+  font-size: 12px;
+  top: -10px;
+  left: 10px;
+  background-color: white;
 }
 .form-group input {
-    padding: 15px;
-    border: 1px solid black;
+  padding: 15px;
+  border: 1px solid black;
 }
 .resetPass p {
-    text-decoration: underline;
-    color: black;
-    font-weight: 500;
+  text-decoration: underline;
+  color: black;
+  font-weight: 500;
 }
 </style>
